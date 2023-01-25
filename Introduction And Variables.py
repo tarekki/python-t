828c566e7f5b633aa051d3977b2a59803cfd7f54
tarekki <123568592+tarekki@users.noreply.github.com>
2023-01-25 21:36:50 +0300

    Add files via upload

---
+++ Introduction And Variables.py
@@ -0,0 +1,15 @@
+name = '"name : tarek"'
+age = '"age : 23"'
+country = '"country : damascus"'
+print(name)
+print(age)
+print(country)
+print(type(name))
+print(type(age))
+print(type(country))
+name = 'tarek '
+age = '23 '
+country = 'damascus '
+print("hello, my name is " + name + "and i am " + age + "years old " + "and i live in " + country)
+                           
+
