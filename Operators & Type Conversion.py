873fe7fff4debbfb7b223b97782c10fbf9535ddb
tarekki <123568592+tarekki@users.noreply.github.com>
2023-01-28 19:41:25 +0300

    Add files via upload

---
+++ Operators & Type Conversion.py
@@ -0,0 +1,15 @@
+x,y=5,6
+print(f"{x<=y}\n{x<y}\n{y>x}\n{y>=x}")
+print(f"{x>y}\n{x>=y}\n{y<x}\n{y<=x}")
+java=50
+c=50
+python=50
+print(java == c == python)
+num_one=10
+num_two=20
+num=20
+print(num>num_one)
+print(num>(num_one and num_two))
+a,b=10,20
+c=a+b
+print(f"{c}\n{c**3}\n{(c**3)%26000}\n{((c**3)%26000)/5}\n{type(((c**3)%26000)/5)}\n{type(str(((c**3)%26000)/5))}")
